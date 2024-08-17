7f761202336934f94829dc83839d25438178a168
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2024-08-17 18:39:14 -0500

    Level 5

---
+++ GMTK24-3/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject toSpawn;
+    public float spawnCooldown = 2;
+
+    void Start()
+    {
+        StartCoroutine(SpawnCountdown());
+    }
+
+    private IEnumerator SpawnCountdown()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnCooldown); 
+            if(spawnCooldown >= 0.8) spawnCooldown -= 0.08f;
+
+            Instantiate(toSpawn,new Vector2(
+                Random.Range(-8,8),
+                5
+            ), Quaternion.identity);
+        }
+    }
+}
