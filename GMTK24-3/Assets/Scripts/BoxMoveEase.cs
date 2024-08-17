246e839a33c1c5287ef3d739d54177171c0a457e
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2024-08-17 15:17:29 -0500

    Script clean up

--- GMTK24-3/Assets/Scripts/BoxMoveEase.cs
+++ GMTK24-3/Assets/Scripts/BoxMoveEase.cs
@@ -1,21 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxMoveEase : MonoBehaviour
 {
-     public float baseDrag = 10f;  // The default drag when no force is applied
-    public float minDrag = 0.5f;  // Minimum drag when the heaviest object is colliding
-    public float maxMass = 20f;   // The mass that results in the minimum drag
-
     public float forceMultiplier = 10f;
-
     Rigidbody2D boxRigidBody2D;
-    // Start is called before the first frame update
+
     void Start()
     {
         boxRigidBody2D = gameObject.GetComponent<Rigidbody2D>();
-        boxRigidBody2D.drag = baseDrag;
     }
 
      void OnCollisionEnter(Collision collision)
@@ -28,11 +20,5 @@ public class BoxMoveEase : MonoBehaviour
             Vector3 force = collision.impulse.normalized * otherRb.mass * forceMultiplier;
             boxRigidBody2D.AddForce(force);
         }
-    }
-
-     void OnCollisionExit(Collision collision)
-    {
-        // Reset the drag when the collision ends
-        boxRigidBody2D.drag = baseDrag;
     }
 }
